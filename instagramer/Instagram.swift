97868c9ec6a795a21e8f530cd18db8e8315fe333
Yohei <yoohei2323@gmail.com>
2015-10-16 16:38:17 +0900

    [add] struct Instagram

---
+++ instagramer/Instagram.swift
@@ -0,0 +1,54 @@
+//
+//  Instagram.swift
+//  instagramer
+//
+//  Created by Yohei Fusayasu on 10/16/15.
+//  Copyright © 2015 instagramers. All rights reserved.
+//
+
+import Alamofire
+import UIKit
+
+struct Instagram {
+    enum Router: URLRequestConvertible{
+        case PopularPhotos(String, String)
+        case requestOauthCode
+        
+        static let baseURLString = "https://api.instagram.com"
+        static let clientID = "b4332c7fc1da41ce92d7b60ff668f32a"
+        static let redirectURI = "http://www.example.com/"
+        static let clientSecret = "e7099308c8fa43c29a2afd66e2266c7a"
+        
+        var URLRequest: NSMutableURLRequest {
+            let result: (path: String, parameters: [String: AnyObject]?) = {
+                switch self {
+                case .PopularPhotos (let userID, let accessToken):
+                    let params = ["access_token": accessToken]
+                    let pathString = "/v1/users/" + userID + "/media/recent"
+                    return (pathString, params)
+                    
+                case .requestOauthCode:
+                    let pathString = "/oauth/authorize/?client_id=" + Router.clientID + "&redirect_uri=" + Router.redirectURI + "&response_type=code"
+                    return (pathString, nil)
+                }
+                }()
+            
+            let BaeseURL = NSURL(string: Router.baseURLString)!
+            let URLRequest = NSURLRequest(URL: BaeseURL.URLByAppendingPathComponent(result.path))
+            let encoding = Alamofire.ParameterEncoding.URL
+            return encoding.encode(URLRequest, parameters: result.parameters).0
+        }
+        
+        static func requestAccessTokenURLStringAndParms(code: String) -> (URLString: String, Params: [String: AnyObject]) {
+            let params = ["client_id": Router.clientID, "client_secret": Router.clientSecret, "grant_type": "authorization_code", "redirect_uri": Router.redirectURI, "code": code]
+            let pathString = "/oauth/access_token"
+            let urlString = Instagram.Router.baseURLString + pathString
+            return (urlString, params)
+        }
+
+    }
+}
+
+extension Alamofire.Request {
+    
+}
