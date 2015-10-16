a9167ef7d1627f2e62242d7b63e6c09c02e19e36
Yohei <yoohei2323@gmail.com>
2015-10-16 17:18:28 +0900

    [add] First launch Storyboard

---
+++ instagramer/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  instagramer
+//
+//  Created by Yohei Fusayasu on 10/16/15.
+//  Copyright © 2015 instagramers. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    @IBOutlet weak var navBar: UINavigationBar!
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
