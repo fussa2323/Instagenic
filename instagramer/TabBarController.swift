81c0cb4a63d22eb94faded5432d2afb52161fda8
Yohei <yoohei2323@gmail.com>
2015-10-17 17:24:25 +0900

    [add] Tabbarcontroller

--- instagramer/TabBarController.swift
+++ instagramer/TabBarController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class TabBarController: UITabBarController {
 
+    let NSUSERDEFAULT_FIRST_TIME = "isFirstTimeDone"
+    var isLogin = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        self.segueToFirstLaunch()
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    //---------------------------
+    //# MARK: - Segue
+    //---------------------------
+    
+    func segueToFirstLaunch() {
+        let storyboard: UIStoryboard = UIStoryboard(name: "FirstLaunch", bundle: nil)
+        let firstLaunchViewController = storyboard.instantiateViewControllerWithIdentifier("FirstLaunch")
+        self.presentViewController(firstLaunchViewController, animated: true, completion: nil)
     }
 }
