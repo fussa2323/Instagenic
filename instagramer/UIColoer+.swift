c5e1d8f2fba09c0dc3085c493d300adfc5186f21
Yohei <yoohei2323@gmail.com>
2015-10-30 17:38:40 +0900

    [add]UIColoerのextensionを追加

---
+++ instagramer/UIColoer+.swift
@@ -0,0 +1,28 @@
+//
+//  UIColoer+.swift
+//  instagramer
+//
+//  Created by Yohei Fusayasu on 10/30/15.
+//  Copyright © 2015 instagramers. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class func mainColor() -> UIColor {
+        return UIColor.rgbColor(0x3f729b)
+    }
+    
+    class func textColor() -> UIColor {
+        return UIColor.rgbColor(0xFFFFFF)
+    }
+    
+    class func rgbColor(rgbValue: UInt, alpha: Float = 1.0) -> UIColor {
+        return UIColor(
+            red:   CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >>  8) / 255.0,
+            blue:  CGFloat( rgbValue & 0x0000FF)        / 255.0,
+            alpha: CGFloat(alpha)
+        )
+    }
+}
