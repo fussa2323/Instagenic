172318bc9f365cba9630c1bec6510dfd503e20fe
Yohei <yoohei2323@gmail.com>
2015-11-03 23:24:57 +0900

    [add] ViewControllerファイルの追加

---
+++ instagramer/MyPageGridViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MyPageGridViewController.swift
+//  instagramer
+//
+//  Created by Yohei Fusayasu on 11/3/15.
+//  Copyright © 2015 instagramers. All rights reserved.
+//
+
+import UIKit
+
+class MyPageGridViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
---
+++ instagramer/MyPageListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MyPageListViewController.swift
+//  instagramer
+//
+//  Created by Yohei Fusayasu on 11/3/15.
+//  Copyright © 2015 instagramers. All rights reserved.
+//
+
+import UIKit
+
+class MyPageListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
