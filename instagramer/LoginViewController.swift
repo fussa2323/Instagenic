a0652fd4619e550a4cd9dc9f044d6c5853a84f38
Yohei <yoohei2323@gmail.com>
2015-11-09 21:32:01 +0900

    [add] delegate

--- instagramer/LoginViewController.swift
+++ instagramer/LoginViewController.swift
@@ -22,6 +22,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        webView.delegate = self
     }
     
     override func viewWillAppear(animated: Bool) {
