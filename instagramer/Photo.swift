09f3b0ad3a82fc13c15fe25ff335355a0b973562
Yohei <yoohei2323@gmail.com>
2015-11-15 16:22:49 +0900

    [add] Photoモデル

--- instagramer/Photo.swift
+++ instagramer/Photo.swift
@@ -11,7 +11,53 @@ import Alamofire
 import ObjectMapper
 import RealmSwift
 import SDWebImage
+import FastImageCache
 
 
-class Photo: Object {
+class Photo: NSObject, FICEntity {
+    
+    var UUID: String {
+        let imageName = sourceImageURL.lastPathComponent!
+        let UUIDBytes = FICUUIDBytesFromMD5HashOfString(imageName)
+        return FICStringWithUUIDBytes(UUIDBytes)
+    }
+    
+    var sourceImageUUID: String {
+        return UUID
+    }
+    
+    var sourceImageURL: NSURL
+    var request: Alamofire.Request?
+    
+    init(sourceImageURL: NSURL) {
+        self.sourceImageURL = sourceImageURL
+        super.init()
+    }
+
+    required override init() {
+        fatalError("init() has not been implemented")
+    }
+    
+    override func isEqual(object: AnyObject?) -> Bool {
+        return (object as! Photo).UUID == self.UUID
+    }
+    
+    func sourceImageURLWithFormatName(formatName: String!) -> NSURL! {
+        return sourceImageURL
+    }
+    
+    func drawingBlockForImage(image: UIImage!, withFormatName formatName: String!) -> FICEntityImageDrawingBlock! {
+        
+        let drawingBlock:FICEntityImageDrawingBlock = {
+            (context:CGContextRef!, contextSize:CGSize) in
+            var contextBounds = CGRectZero
+            contextBounds.size = contextSize
+            CGContextClearRect(context, contextBounds)
+            
+            UIGraphicsPushContext(context)
+            image.drawInRect(contextBounds)
+            UIGraphicsPopContext()
+        }
+        return drawingBlock
+    }
 }
