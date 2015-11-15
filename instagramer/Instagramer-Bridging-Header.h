0f407d34d13395f5c50c5b12fae6bfa13060ae47
Yohei <yoohei2323@gmail.com>
2015-11-15 14:44:10 +0900

    [add] Bridging-Headerの追加

---
+++ instagramer/Instagramer-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Instagramer-Bridging-Header.h
+//  instagramer
+//
+//  Created by Yohei Fusayasu on 11/15/15.
+//  Copyright © 2015 instagramers. All rights reserved.
+//
+
+#ifndef Instagramer_Bridging_Header_h
+#define Instagramer_Bridging_Header_h
+
+#import <SDWebImage/UIImageView+WebCache.h>
+
+#endif /* Instagramer_Bridging_Header_h */
