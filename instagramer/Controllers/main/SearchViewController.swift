7593c91dac686771ff1aaa50218a7be18d30c694
Yohei <yoohei2323@gmail.com>
2015-12-01 22:59:48 +0900

    SearchBarメソッドを追加

--- instagramer/Controllers/main/SearchViewController.swift
+++ instagramer/Controllers/main/SearchViewController.swift
@@ -12,43 +12,54 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.title = "Search"
         self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
         
-        // サーチバーを作成する。
-        let uiSearchBar : UISearchBar = UISearchBar()
-        // 表示する位置と大きさを設定
-        uiSearchBar.frame = CGRectMake(0,0,300,80)
-        // デリゲートの設定先を設定
-        uiSearchBar.delegate = self
-        // 表示するテキストを設定(入力済みにする)
-        uiSearchBar.text = ""
-        // サーチバーのタイトルを設定する。
-        uiSearchBar.prompt = ""
-        // プレスフォルダーを設定する。
-        uiSearchBar.placeholder = "検索"
-        // キャンセルボタン表示を設定(クリアボタン)
-        uiSearchBar.showsCancelButton = true
-        // ブックマークボタン表示を設定
-        uiSearchBar.showsBookmarkButton = false
-        // 検索結果表示ボタン表示を設定
-        uiSearchBar.showsSearchResultsButton = false
-        // テキスト入力時のキーボードのスタイルを設定
-        uiSearchBar.keyboardType = UIKeyboardType.Default
-        // バースタイルをDefaultに設定する.
-        uiSearchBar.searchBarStyle = UISearchBarStyle.Minimal
-        // 検索バーのスタイルを設定
-        uiSearchBar.barStyle = UIBarStyle.Default
-        // 検索バー全体の色を設定
-        uiSearchBar.tintColor = UIColor.mainColor()
         
-        // UINavigationBar上に、UISearchBarを追加
+        // UISearchBar---------------
+
+        let uiSearchBar : UISearchBar = UISearchBar()
+        uiSearchBar.frame = CGRectMake(0,0,300,80)
+        uiSearchBar.delegate = self
+        uiSearchBar.text = "" // 表示するテキストを設定(入力済みにする)
+        uiSearchBar.prompt = "" // サーチバーのタイトルを設定する。
+        uiSearchBar.placeholder = "検索" // プレスフォルダーを設定する。
+        uiSearchBar.showsCancelButton = true // キャンセルボタン表示を設定(クリアボタン)
+        uiSearchBar.showsSearchResultsButton = false // 検索結果表示ボタン表示を設定
+        uiSearchBar.keyboardType = UIKeyboardType.Default // テキスト入力時のキーボードのスタイルを設定
+        uiSearchBar.searchBarStyle = UISearchBarStyle.Minimal // バースタイルをDefaultに設定する.
+        uiSearchBar.barStyle = UIBarStyle.Default // 検索バーのスタイルを設定
+        uiSearchBar.tintColor = UIColor.mainColor() // 検索バー全体の色を設定
+        
         self.navigationItem.titleView = uiSearchBar
         self.navigationItem.titleView!.frame = CGRectMake(0, 0, 320, 44);
+
+        // UISearchBar----------------
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    //---------------------------
+    // MARK: UISearchBar
+    //---------------------------
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        <#code#>
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        <#code#>
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        <#code#>
+    }
+    
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        <#code#>
     }
 }
