facc00b77432f2f6881c448c0711d57d184a8395
Yohei <yoohei2323@gmail.com>
2015-12-06 16:44:50 +0900

    [add] loadFeedData method

--- instagramer/Controllers/main/sub1/FeedListItemViewController.swift
+++ instagramer/Controllers/main/sub1/FeedListItemViewController.swift
@@ -11,9 +11,11 @@ import Alamofire
 import FastImageCache
 import SwiftyJSON
 
-class FeedListItemViewController: UIViewController, UITableViewDelegate{
+class FeedListItemViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
     @IBOutlet weak var tableView: UITableView!
+    let accessToken = CurrentAccount.sharedController.account?.accessToken
+    let username = CurrentAccount.sharedController.account?.userName
     var photos = [Photo]()
     let refreshControl = UIRefreshControl()
     var populatingPhotos = false
@@ -26,7 +28,7 @@ class FeedListItemViewController: UIViewController, UITableViewDelegate{
         super.viewDidLoad()
         
         self.tableView.delegate = self
-//        self.tableView.dataSource = self
+        self.tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,15 +43,70 @@ class FeedListItemViewController: UIViewController, UITableViewDelegate{
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 
+        return photos.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        // 再利用するCellを取得する.
-        let cell = tableView.dequeueReusableCellWithIdentifier("MyCell", forIndexPath: indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier("FeedListItemCell", forIndexPath: indexPath) as! FeedListItemTableViewCell
         
         
         return cell
     }
+    
+    //----------------------------
+    // MARK: Request methods
+    //----------------------------
+    
+    func loadFeedData() {
+        if populatingPhotos {
+            return
+        }
+        
+        populatingPhotos = true
+        
+        Alamofire.request(Instagram.Router.GetUserFeed(self.username!, self.accessToken!, "", ""))
+            .responseJSON { (request, response, result) -> Void in
+                defer {
+                    self.populatingPhotos = false
+                }
+                switch result{
+                    case .Success(let jsonObject):
+                        let json = JSON(jsonObject)
+                    
+                        if (json["meta"]["code"].intValue  == 200) {
+                            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) {
+                                if let urlString = json["pagination"]["next_url"].URL {
+                                    self.nextURLRequest = NSURLRequest(URL: urlString)
+                                } else {
+                                    self.nextURLRequest = nil
+                                }
+                                let photos = json["data"].arrayValue
+                                    
+                                    .filter {
+                                        $0["type"].stringValue == "image"
+                                    }.map({
+                                        Photo(sourceImageURL: $0["images"]["standard_resolution"]["url"].URL!)
+                                    })
+                                
+                                let lastItem = self.photos.count
+                                self.photos.appendContentsOf(photos)
+                                
+                                let indexPaths = (lastItem..<self.photos.count).map { NSIndexPath(forItem: $0, inSection: 0) }
+                                
+                                dispatch_async(dispatch_get_main_queue()) {
+                                    self.tableView.insertRowsAtIndexPaths(indexPaths, withRowAnimation: UITableViewRowAnimation.Automatic)
+                                }
+                                
+                            }
+                    }
+                    
+                    case .Failure(let error):
+                        print(error)
+                }
+        }
+        
+        
+    }
+
 }
